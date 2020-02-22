01630c111497c3de32d8246e4e834da67f38cd98
Leonardo Welter <lw@example.com>
2020-02-22 16:11:28 -0300

    Finalização e documentação da aplicação

--- app/app.php
+++ app/app.php
@@ -1,10 +1,19 @@
 <?php
 
+    // Classe que representa a abstração do Dashboard
+	// Possui Getters e Setters genéricos para recuperar os atributos,
+	// embora os atributos estejam publicos para facilitar o retorno.
 	class Dashboard {
 		public $dataInicio;
 		public $dataFim;
 		public $numeroVendas;
 		public $totalVendas;
+		public $usuariosAtivos;
+		public $usuariosInativos;
+		public $totalReclamacoes;
+		public $totalElogios;
+		public $totalSugestoes;
+		public $totalDespesas;
 
 
 		public function getDataInicio()
@@ -46,14 +55,78 @@
 		{
 			$this->totalVendas = $totalVendas;
 		}
+
+		public function getUsuariosAtivos()
+		{
+			return $this->usuariosAtivos;
+		}
+
+		public function setUsuariosAtivos($usuariosAtivos)
+		{
+			$this->usuariosAtivos = $usuariosAtivos;
+		}
+
+		public function getUsuariosInativos()
+		{
+			return $this->usuariosInativos;
+		}
+
+		public function setUsuariosInativos($usuariosInativos)
+		{
+			$this->usuariosInativos = $usuariosInativos;
+		}
+
+		public function getTotalReclamacoes()
+		{
+			return $this->totalReclamacoes;
+		}
+
+		public function setTotalReclamacoes($totalReclamacoes)
+		{
+			$this->totalReclamacoes = $totalReclamacoes;
+		}
+
+		public function getTotalElogios()
+		{
+			return $this->totalElogios;
+		}
+
+		public function setTotalElogios($totalElogios)
+		{
+			$this->totalElogios = $totalElogios;
+		}
+
+		public function getTotalSugestoes()
+		{
+			return $this->totalSugestoes;
+		}
+
+		public function setTotalSugestoes($totalSugestoes)
+		{
+			$this->totalSugestoes = $totalSugestoes;
+		}
+
+		public function getTotalDespesas()
+		{
+			return $this->totalDespesas;
+		}
+
+		public function setTotalDespesas($totalDespesas)
+		{
+			$this->totalDespesas = $totalDespesas;
+		}
 	}
 
+	// Classe que representa a Conexão com o SGBD
 	class Conexao {
-		private $host = 'localhost';
-		private $dbname = 'dashboard';
-		private $user = 'leonardo';
-		private $pass = 'p4ss*';
+		// Dados de conexão e autenticação com o SGBD
+		private $host = 'localhost'; // Host
+		private $dbname = 'dashboard'; // Database
+		private $user = 'USER'; // Usuário
+		private $pass = 'SENHA'; // Senha
 
+		// Função que realiza a conexão com o SGBD, utilizando os parâmetros inseridos anteriormente
+		// Retorna o objeto da conexão,
 		public function conectar() {
 			try {
 				$conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname", "$this->user", "$this->pass");
@@ -68,6 +141,8 @@
 		}
 	}
 
+	// Classe BD implementa a camada Service da aplicação, responsável pela recuperação dos dados
+	// que estão inseridos no SGBD
 	class BD {
 		private $conexao;
 		private $dashboard;
@@ -99,24 +174,97 @@
 
 			return $stmt->fetch(PDO::FETCH_OBJ)->totalVendas;
 		}
+
+		public function getClientesAtivos() {
+			$query = 'SELECT COUNT(*) AS clientesAtivos FROM tb_clientes WHERE cliente_ativo = 1';
+
+			$stmt = $this->conexao->prepare($query);
+			$stmt->execute();
+
+			return $stmt->fetch(PDO::FETCH_OBJ)->clientesAtivos;
+		}
+
+		public function getClientesInativos() {
+			$query = 'SELECT COUNT(*) AS clientesInativos FROM tb_clientes WHERE cliente_ativo = 0';
+
+			$stmt = $this->conexao->prepare($query);
+			$stmt->execute();
+
+			return $stmt->fetch(PDO::FETCH_OBJ)->clientesInativos;
+		}
+
+	    // 1 = reclamacao | 2 = sugestão | 3 = elogio
+		public function getTotalReclamacoes() {
+			$query = 'SELECT COUNT(*) as totalReclamacoes FROM tb_contatos WHERE tipo_contato = 1';
+
+			$stmt = $this->conexao->prepare($query);
+			$stmt->execute();
+
+			return $stmt->fetch(PDO::FETCH_OBJ)->totalReclamacoes;
+		}
+
+		public function getTotalElogios() {
+			$query = 'SELECT COUNT(*) as totalElogios FROM tb_contatos WHERE tipo_contato = 3';
+
+			$stmt = $this->conexao->prepare($query);
+			$stmt->execute();
+
+			return $stmt->fetch(PDO::FETCH_OBJ)->totalElogios;
+		}
+
+		public function getTotalSugestoes() {
+			$query = 'SELECT COUNT(*) as totalSugestoes FROM tb_contatos WHERE tipo_contato = 2';
+
+			$stmt = $this->conexao->prepare($query);
+			$stmt->execute();
+
+			return $stmt->fetch(PDO::FETCH_OBJ)->totalSugestoes;
+		}
+
+		public function getTotalDespesas() {
+			$query = 'SELECT SUM(total) AS totalDespesas FROM tb_despesas WHERE data_despesa BETWEEN :dataInicio AND :dataFim';
+
+			$stmt = $this->conexao->prepare($query);
+			$stmt->bindValue(':dataInicio', $this->dashboard->getDataInicio());
+			$stmt->bindValue(':dataFim', $this->dashboard->getDataFim());
+			$stmt->execute();
+
+			return $stmt->fetch(PDO::FETCH_OBJ)->totalDespesas;
+		}
 	}
 
+	// Instanciação das classes Dashboard e Conexao
 	$dashboard = new Dashboard();
-
 	$conexao = new Conexao();
 
+	// Separa a data recebida via GET para tratamento
 	$competencia = explode('-',$_GET['competencia']);
 
+	// Divide o array do Explode em variáveis separadas
 	$ano = $competencia[0];
 	$mes = $competencia[1];
 
+	// Calcula quantos dias tem no mês selecionado
 	$diasMes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
 
+	// Atribui na instancia de Dashboard a data de inicio e fim da competencia
 	$dashboard->setDataInicio($ano.'-'.$mes.'-01');
 	$dashboard->setDataFim($ano. '-'. $mes . '-'. $diasMes);
 
+	// Instancia de BD para tratar das operações com o BD
 	$bd = new BD($conexao, $dashboard);
 
+	// Atribui na instancia de Dashboard o restante dos atributos através dos métodos
+	// disponibilizados pela Classe BD;
 	$dashboard->setNumeroVendas($bd->getNumeroVendas());
 	$dashboard->setTotalVendas($bd->getTotalVendas());
+	$dashboard->setUsuariosAtivos($bd->getClientesAtivos());
+	$dashboard->setUsuariosInativos($bd->getClientesInativos());
+	$dashboard->setTotalReclamacoes($bd->getTotalReclamacoes());
+	$dashboard->setTotalElogios($bd->getTotalElogios());
+	$dashboard->setTotalSugestoes($bd->getTotalSugestoes());
+	$dashboard->setTotalDespesas($bd->getTotalDespesas());
+
+	// Converte a instancia de Dashboard para JSON e imprime na tela para poder ser requisitado
+	// pelo AJAX e aplicado no Front End;
 	echo json_encode($dashboard);
--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 $(document).ready(() => {
 
+    // Altera o conteúdo da página de forma assíncrona utilizando AJAX
+    // Invocado pelo click do botão no menu lateral
     $('#documentacao').on('click', () => {
         $('#pagina').load('../documentacao.html');
-    })
+    });
 
     $('#suporte').on('click', () => {
         $('#pagina').load('../suporte.html');
-    })
+    });
 
+    // Realiza a atualização e/ou alteração dos dados apresentados no Dashboard
+    // com base nas Queries implementadas em app.php que retorna um objeto JSON
+    // invocado com base na troca de valor do Select que representa a competência
     $('#competencia').on('change', e => {
         let competencia = $(e.target).val();
 
+        // Chamada AJAX do jQuery
+        // Substitui o HTML dos cards pelo retorno do objeto JSON de app.php
         $.ajax({
             type: 'GET',
             url: '../app/app.php',
@@ -19,10 +26,15 @@ $(document).ready(() => {
             success: dados => {
                 $('#numeroVendas').html(dados.numeroVendas);
                 $('#totalVendas').html(dados.totalVendas);
-                console.log(dados.numeroVendas + ' ' + dados.totalVendas)
+                $('#clientesAtivos').html(dados.usuariosAtivos);
+                $('#clientesInativos').html(dados.usuariosInativos);
+                $('#totalReclamacoes').html(dados.totalReclamacoes);
+                $('#totalElogios').html(dados.totalElogios);
+                $('#totalSugestoes').html(dados.totalSugestoes);
+                $('#totalDespesas').html(dados.totalDespesas);
             } ,
             error: erro => {console.log(erro)}
         });
     })
 
-})
+});
